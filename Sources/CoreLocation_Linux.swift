14f97b646d95cb7ed33b468dafefe59cdad2aa09
ryanpeggs <ryanpeggs@Ryans-MacBook-Pro-2.local>
2017-05-14 16:27:29 +0100

    Remove tests

--- Sources/CoreLocation_Linux.swift
+++ Sources/CoreLocation_Linux.swift
@@ -1,4 +1,30 @@
-struct CoreLocation_Linux {
+//
+//  CoreLocation_Linux.swift
+//  CoreLocation_Linux
+//
+//  Created by Ryan Peggs on 14/05/17.
+//  Copyright © 2017 CRRInnovations. All rights reserved.
+//
 
-    var text = "Hello, World!"
+import Foundation
+
+typealias CLLocationDegrees = Double
+typealias CLLocationAccuracy = Double
+typealias CLLocationDistance = Double
+typealias CLLocationSpeed = Double
+typealias CLLocationDirection = Double
+
+public struct CLLocationCoordinate2D {
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+    init() {
+        latitude = 0
+        longitude = 0
+    }
+    
+    init(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
+
--- Tests/CoreLocation-LinuxTests/CoreLocation_LinuxTests.swift
+++ Tests/CoreLocation-LinuxTests/CoreLocation_LinuxTests.swift
@@ -2,14 +2,7 @@ import XCTest
 @testable import CoreLocation_Linux
 
 class CoreLocation_LinuxTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(CoreLocation_Linux().text, "Hello, World!")
-    }
 
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    static var allTests = []
+    
 }
